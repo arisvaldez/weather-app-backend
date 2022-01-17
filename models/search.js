d3be5ba7c68641a4e823b302fc5b91ac52f67cfc
aris valdez <samus45@hotmail.es>
2022-01-16 22:57:26 -0400

    added dotenv package and .env file

--- models/search.js
+++ models/search.js
@@ -2,17 +2,37 @@ const axios = require('axios');
 
 class Search {
   history = ['otawa', 'santiago', 'washintong'];
+  languaje = 'es';
+  limit = 5;
+  base_url = '';
+
+  get paramsMapBox() {
+    return {
+      access_token:
+        'pk.eyJ1IjoiYXJpc3ZhbGRleiIsImEiOiJja3NlMGpsNDAwdnV1Mm9xa2Z4dmp3YXRuIn0.LuHeuk02dA_qHZ0D3lZY-g',
+      language: 'es',
+      limit: 5,
+    };
+  }
 
   constructor() {
     //TODO: leerdb
   }
 
   async city(name) {
-   const response = await axios.get('https://reqres.in/api/users/2');
-   console.log(response.data
-    )
+    try {
+      const instace = axios.create({
+        baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${name}.json`,
+        params: this.paramsMapBox,
+      });
 
-    return [];
+      const response = await instace.get();
+      console.log(response.data);
+      return [];
+    } catch (ex) {
+      console.log(ex);
+      return [];
+    }
   }
 }
 
